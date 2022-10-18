7587294f7d73af683bbdd1dc529a8f00e310688c
Ori Ochayon <101987673+orioch@users.noreply.github.com>
2022-10-18 19:43:54 +0300

    replace the table to MUI table

--- src/components/tableHeader.jsx
+++ src/components/tableHeader.jsx
@@ -1,12 +1,14 @@
 import { useSelector, useDispatch } from "react-redux";
 import { sortCryptoArray } from "../redux/features/cryptoDataSlice";
 import { BiUpArrow, BiDownArrow } from "react-icons/bi";
+import { Th } from "react-super-responsive-table";
+import { TableCell } from "@mui/material";
 
 export default function TableHeader({ children, id }) {
   const dispatch = useDispatch();
   const { sort } = useSelector((store) => store.cryptoData);
   return (
-    <th onClick={() => dispatch(sortCryptoArray(id))}>
+    <TableCell onClick={() => dispatch(sortCryptoArray(id))}>
       <div className="cell">
         <div className="name-text"> {children}</div>
         {sort.prop == id ? (
@@ -17,6 +19,6 @@ export default function TableHeader({ children, id }) {
           )
         ) : null}
       </div>
-    </th>
+    </TableCell>
   );
 }
